c5c1c3b52ea797333d0a660810380e0be3d2e2aa
Marquis Kurt <software@marquiskurt.net>
2021-02-22 22:13:28 -0500

    ✨ Add basic player movement

--- src/logic/driver.py
+++ src/logic/driver.py
@@ -5,6 +5,7 @@
 #
 
 from os import path
+from src.logic.player import Player
 import pygame
 from src.assets import Tilesheet, ColorPalette
 
@@ -12,14 +13,13 @@ from src.assets import Tilesheet, ColorPalette
 class Lifelight():
     """The main application class for the game."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Set up the game's canvas, colors, tilesheets, and event listeners."""
 
         self.canvas = pygame.display.set_mode((1280, 720))
         pygame.display.set_caption("Lifelight")
 
         self.frame_limiter = pygame.time.Clock()
-        self.frame_limiter.tick(60)
 
         self.palette = ColorPalette(path.join("assets", "adventure28.gpl"))
         self.palette.assign_color_name("DARK_BLACK", "091d21")
@@ -27,18 +27,27 @@ class Lifelight():
         self.ts_structures = Tilesheet(
             path.join("assets", "tilesheets", "struct.png"), (48, 48), (12, 16))
 
-    def manage_game_events(self):
-        """Loop through the game events and return if the game should quit."""
+        self.player = Player((200, 200), 4)
+
+    def manage_game_events(self) -> bool:
+        """Manage the primary game events such as quitting, player movement, etc."""
+        self.frame_limiter.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
+
+        pressed = pygame.key.get_pressed()
+        self.player.update_position(pressed)
+
         return True
 
-    def update_canvas(self):
+    def update_canvas(self) -> None:
         """Update the contents of the canvas."""
         self.canvas.fill(self.palette.get_color("DARK_BLACK"))
-        self.canvas.blit(self.ts_structures.get_tile(2, 3), (200, 200))
+        self.canvas.blit(self.ts_structures.get_tile(
+            2, 3), self.player.position)
 
-    def render(self):
+    def render(self) -> None:
         """Render the changes onto the screen."""
         pygame.display.update()
