181a7d3ec1e66bdfa7ae874245547cf79ed6601e
Marquis Kurt <software@marquiskurt.net>
2021-02-23 22:35:18 -0500

    ✨ Draw tiles from level data to screen

--- src/logic/driver.py
+++ src/logic/driver.py
@@ -7,6 +7,7 @@
 from os import path
 from typing import Tuple
 from src.logic.player import Player
+from src.logic.levels import Level
 import pygame
 from src.assets import Tilesheet, ColorPalette, asset_path
 
@@ -27,9 +28,10 @@ class Lifelight():
         self.palette.assign_color_name("DARK_BLACK", "091d21")
 
         self.ts_structures = Tilesheet(
-            asset_path("assets/tilesheets/struct.png"), (48, 48), (12, 16))
+            asset_path("assets/tilesheets/struct01.png"), (48, 48), (12, 16))
 
         self.player = Player((200, 200), 4)
+        self.level = Level(asset_path("data/random01.lvl"))
 
     def manage_game_events(self) -> bool:
         """Manage the primary game events such as quitting, player movement, etc."""
@@ -46,9 +48,28 @@ class Lifelight():
 
     def update_canvas(self) -> None:
         """Update the contents of the canvas."""
+
+        # Fill the canvas with a black-like color.
         self.canvas.fill(self.palette.get_color("DARK_BLACK"))
-        self.canvas.blit(self.ts_structures.get_tile(
-            2, 3), self.player.position)
+
+        # Set up the tile sequencers.
+        tx, ty = 16, 16  # TODO: Get these tiles centered!
+        width, height = self.ts_structures.tile_size
+
+        # Iterate through all of the tilemap rows, filling the tilemap slowly.
+        for row in self.level.tiles:
+
+            # Fill in the tile with the appropriate tileset image at that position, or don't fill anything if the tile
+            # is an "air" tile.
+            for cx, cy in row:
+                if cx != -1 and cy != -1:
+                    self.canvas.blit(
+                        self.ts_structures.get_tile(cx, cy), (tx, ty))
+                tx += width
+
+            # Move to the next row and reset the X position.
+            ty += height
+            tx = 16
 
     def render(self) -> None:
         """Render the changes onto the screen."""
